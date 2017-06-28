48a62a0ebe1200a929d6c6cbe89d1e5b8c5c6602
Tim Veletta <tim.veletta@gmail.com>
2017-06-28 11:29:49 +0800

    Finished Lambda function.

--- lambda/index.js
+++ lambda/index.js
@@ -1,12 +1,61 @@
 var request = require('request');
+var requestPromise = require('request-promise-native');
 
 exports.handler = (event, context, callback) => {
     process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
 
-    request.get(`https://developers.zomato.com/api/v2.1/search?lat=${position.coords.latitude}&lon=${position.coords.longitude}&radius=${radius}&sort=real_distance`, 
-        { 'headers': { 'user-key': process.env['ZOMATO_API_KEY'] } },
-        (res, err) => {
-            console.log(res);
-            console.error(err);
+    let lat = event.queryStringParameters ? event.queryStringParameters.lat : event.lat;
+    let long = event.queryStringParameters ? event.queryStringParameters.long : event.long;
+    let rad = event.queryStringParameters ? event.queryStringParameters.rad : event.rad;
+
+    console.log("Request: " + JSON.stringify(event));
+    console.log(`Latitude: ${lat}; Longitude: ${long}; Radius: ${rad}`);
+
+    let createResponse = (body) => {
+        return { 
+            "statusCode": 200, 
+            "body": JSON.stringify({
+                "restaurants": body.restaurants.map(r => {
+                    return {
+                        "id": r.restaurant.id,
+                        "name": r.restaurant.name,
+                        "address": r.restaurant.location.address,
+                        "cuisines": r.restaurant.cuisines.split(/,/).map(c => {
+                            return c.trim();
+                        }),
+                        "image": r.restaurant.thumb
+                    };
+                })
+            })
+        }
+    };
+
+    request.get(`https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${long}&radius=${rad}&sort=real_distance`, 
+        {headers: {'user-key': process.env['ZOMATO_API_KEY']}},
+        (err, res) => {
+            let body = JSON.parse(res.body);
+
+            console.log(`Results Found: ${body.results_found}`);
+
+            if(body.results_found > 20) {
+                let additionalRequests = [];
+                for(let i = 20; i < body.results_found && i <= 100; i += 20) {
+                    additionalRequests.push(requestPromise.get(`https://developers.zomato.com/api/v2.1/search?lat=${lat}&lon=${long}&radius=${rad}&sort=real_distance&start=${i}`, 
+                        {headers: {'user-key': process.env['ZOMATO_API_KEY']}}));
+                }
+                Promise.all(additionalRequests)
+                    .then(values => {
+                        for(let j = 0; j < values.length; j++) {
+                            let value = JSON.parse(values[j]);
+                            body.restaurants = body.restaurants.concat(value.restaurants);
+                        }
+
+                        console.log(`Results Returned: ${body.restaurants.length}`);
+                        callback(err, createResponse(body));
+                    });
+            } else {
+                console.log(`Results Returned: ${body.restaurants.length}`);
+                callback(err, createResponse(body));
+            }
         });
 };
